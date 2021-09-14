e5acc416aced2d2370cef7b08b63cbe8f84fa3c2
Thijs <gitkraken@thd.ninje>
2021-09-14 13:21:51 +0200

    Cookiecutter Django initial setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# fires.watch
+F.I.R.E.S.watch is an online too to quickly calculate your F.I.R.E Stock portfolio.
---
+++ fires_watch/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from fires_watch.users.models import User
+from fires_watch.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
