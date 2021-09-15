08e00f3b59925ef56396428555e6497854e30471
Raoul Capello <72148841+raoulcapello@users.noreply.github.com>
2021-09-15 11:56:15 +0200

    Add Around Bootstrap template basic elements
    - CSS, JS, images

---
+++ fires_watch/static/js/page-load-spinner.js
@@ -0,0 +1,12 @@
+/* DevFolio specific Javascript */
+
+// Page loading script
+(function () {
+  window.onload = function () {
+    var preloader = document.querySelector(".page-loading");
+    preloader.classList.remove("active");
+    setTimeout(function () {
+      preloader.remove();
+    }, 2000);
+  };
+})();
