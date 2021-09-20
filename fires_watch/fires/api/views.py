ddc6bdd1bc17da8e3214ecb002f9d01429b8f7ce
Raoul Capello <72148841+raoulcapello@users.noreply.github.com>
2021-09-20 16:38:01 +0200

    Allow unauthenticated users to use Calculator

--- fires_watch/fires/api/views.py
+++ fires_watch/fires/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
@@ -7,7 +8,7 @@ from fires_watch.fires.api.serializers import FiresCalculateSerializer
 
 
 class FiresViewSet(GenericViewSet):
-    @action(detail=False, methods=["POST"])
+    @action(detail=False, methods=["POST"], permission_classes=[AllowAny])
     def calculate(self, request):
         serializer = FiresCalculateSerializer(
             context={"request": request}, data=request.data
@@ -16,5 +17,6 @@ class FiresViewSet(GenericViewSet):
             return Response(status=status.HTTP_200_OK, data=serializer.data)
         else:
             return Response(
-                status=status.HTTP_400_BAD_REQUEST, data=serializer.error_messages
+                status=status.HTTP_400_BAD_REQUEST,
+                data=serializer.error_messages,
             )
